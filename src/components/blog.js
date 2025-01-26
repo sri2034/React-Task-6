0fc4e844c389951d430a414a1a59c3506ccc2283
Kurukuri Pavan Sriram <143617953+sri2034@users.noreply.github.com>
2025-01-26 13:09:42 +0530

    Update blog.js

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,21 +1,21 @@
-import React from 'react';
+// import React from 'react';
 
-function Blog() {
-    return (
-        <div>
-            <section className="ftco-section bg-light" id="blog-section">
-                <div className="container">
-                    <div className="row justify-content-center">
-                        <div className="col-md-7 heading-section text-center">
-                            <span className="subheading">Blog</span>
-                            <h2 className="mb-4">NO BLOGS YET</h2>
-                            <h3 className="mb-4">VISIT MY PAGE SOME OTHER TIME FOR INTERESTING BLOGS</h3>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </div>
-    );
-}
+// function Blog() {
+//     return (
+//         <div>
+//             <section className="ftco-section bg-light" id="blog-section">
+//                 <div className="container">
+//                     <div className="row justify-content-center">
+//                         <div className="col-md-7 heading-section text-center">
+//                             <span className="subheading">Blog</span>
+//                             <h2 className="mb-4">NO BLOGS YET</h2>
+//                             <h3 className="mb-4">VISIT MY PAGE SOME OTHER TIME FOR INTERESTING BLOGS</h3>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </section>
+//         </div>
+//     );
+// }
 
-export default Blog;
+// export default Blog;
